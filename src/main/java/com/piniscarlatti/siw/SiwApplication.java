81661f2cce0b3b53a253fe814053849aba3ebd10
valentino.sacco <valentino.sacco@pronetics.it>
2019-05-24 13:45:06 +0200

    Message overloaded

---
+++ src/main/java/com/piniscarlatti/siw/SiwApplication.java
@@ -0,0 +1,13 @@
+package com.piniscarlatti.siw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwApplication.class, args);
+	}
+
+}
