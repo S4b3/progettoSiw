bab7ffdd0202e157e4a439174643389e6164de46
valentino.sacco <valentino.sacco@pronetics.it>
2019-05-24 16:52:40 +0200

    Inizializzata table funzionario e import di Lombok

--- src/main/java/com/piniscarlatti/siw/SiwApplication.java
+++ src/main/java/com/piniscarlatti/siw/SiwApplication.java
@@ -8,6 +8,7 @@ public class SiwApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SiwApplication.class, args);
+
 	}
 
 }
---
+++ src/main/java/com/piniscarlatti/siw/model/Funzionario.java
@@ -0,0 +1,23 @@
+package com.piniscarlatti.siw.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@EqualsAndHashCode
+public class Funzionario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long _id;
+    private String username;
+    private String password;
+
+
+}
