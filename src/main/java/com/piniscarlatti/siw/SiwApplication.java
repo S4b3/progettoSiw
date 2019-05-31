4b08d20ae73e1de411f8282f37e975385d242a5e
Leonardo Sanò <leonardo.sano@sourcesense.com>
2019-06-01 01:26:03 +0200

    aggiunta visualizzazione di tutti i fotografi

--- src/main/java/com/piniscarlatti/siw/SiwApplication.java
+++ src/main/java/com/piniscarlatti/siw/SiwApplication.java
@@ -1,13 +1,17 @@
 package com.piniscarlatti.siw;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EntityScan
+@EnableTransactionManagement
 public class SiwApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/piniscarlatti/siw/controller/GalleryController.java
+++ src/main/java/com/piniscarlatti/siw/controller/GalleryController.java
@@ -6,12 +6,15 @@ import com.piniscarlatti.siw.entity.Fotografo;
 import com.piniscarlatti.siw.repository.FotografoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -32,24 +35,30 @@ public class GalleryController implements WebMvcConfigurer {
         return "gallery.html";
     }
 
+    //aggiunta di un fotografo
     @GetMapping("/photographers")
-    public String showForm(Fotografo fotografo){
+    public String showForm(Fotografo fotografo,Model model){
         return "formFotografo";
     }
 
     @PostMapping("/photographers")
-    public String insertFotografo(@Valid Fotografo fotografo, BindingResult bindingResult){
+    public String insertFotografo(@Valid Fotografo fotografo, BindingResult bindingResult,Model model){
 
         try{
             fotografo.setAlbumBase();
             fotografoRepository.save(fotografo);
+
         }catch (Exception e){
+
             return "formFotografo";
         }
 
-        return "redirect:/results";
+        List<Fotografo> fotografi = new ArrayList<>(fotografoRepository.findAll());
+        model.addAttribute("alfa",fotografi);
+        return "results";
     }
 
+    //cancellazione di tutti i fotografi dal db
     @GetMapping("/deletephotographers")
     public String showDeleteButton(){
         return "deleteFotografo";
@@ -62,24 +71,12 @@ public class GalleryController implements WebMvcConfigurer {
         return "results";
     }
 
-    @GetMapping("/photographers/{name}")
-    @ResponseBody
-    public List<Fotografo> findByName(@PathVariable String name){
-        return fotografoRepository.findByNome(name);
-    }
-
     @GetMapping("/photographers/all")
-    @ResponseBody
-    public List<Fotografo> findAll(){
-        return fotografoRepository.findAll();
+    public String loadRes(Model model){
+        List<Fotografo> fotografi = new ArrayList<>(fotografoRepository.findAll());
+        model.addAttribute("alfa",fotografi);
+        return "results";
     }
-
-
-
-
-
-
-
 
     @GetMapping("/albums")
     public String getAlbumsGallery(){
